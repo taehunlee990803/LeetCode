842243163fae9a977de7d7a628b7f55a89a37b4b
Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2023-09-14 20:54:58 -0400

    Time: 32 ms (64.60%), Space: 13.3 MB (96.99%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -4,45 +4,60 @@ class Solution(object):
         :type nums1: List[int]
         :type nums2: List[int]
         :rtype: List[int]
+        
+        
         """
-        result = []
-        find = True
+        
+        
+        d = {}
+        st = []
+        ans = []
+        
+        for num in nums2:
+            while len(st) and st[-1] < num:
+                d[st.pop()] = num
+            st.append(num)
+        for x in nums1:
+            ans.append(d.get(x,-1))
+        return ans
+#         result = []
+#         find = True
       
          
-        # for i in range(0,len(nums1)):
-        #     find = True
-        #     for j in range(0,len(nums2)):
-        #         if nums2[j] == nums1[i]:
-        #             for k in range(j,len(nums2)):
-        #                 if nums2[k] > nums2[j]:
-        #                     result.append(nums2[k])
-        #                     find = False
-        #                     break
-        #             if find is True:
-        #                 result.append(-1)
-        temp_index = 0
-        for i in range(0,len(nums1)):
+#         # for i in range(0,len(nums1)):
+#         #     find = True
+#         #     for j in range(0,len(nums2)):
+#         #         if nums2[j] == nums1[i]:
+#         #             for k in range(j,len(nums2)):
+#         #                 if nums2[k] > nums2[j]:
+#         #                     result.append(nums2[k])
+#         #                     find = False
+#         #                     break
+#         #             if find is True:
+#         #                 result.append(-1)
+#         temp_index = 0
+#         for i in range(0,len(nums1)):
          
-            find = True
-            temp_index = nums2.index(nums1[i])
+#             find = True
+#             temp_index = nums2.index(nums1[i])
     
-            for j in range(temp_index, len(nums2)):
-                if nums2[j] > nums1[i]:
-                    result.append(nums2[j])
-                    find = False
-                    break
-            if find is True:
-                result.append(-1)
+#             for j in range(temp_index, len(nums2)):
+#                 if nums2[j] > nums1[i]:
+#                     result.append(nums2[j])
+#                     find = False
+#                     break
+#             if find is True:
+#                 result.append(-1)
                     
             
-#             for j in range(0,len(nums2)):
-#                 if nums2[j] == nums1[i]:
-#                     for k in range(j,len(nums2)):
-#                         if nums2[k] > nums2[j]:
-#                             result.append(nums2[k])
-#                             find = False
-#                             break
-#                     if find is True:
-#                         result.append(-1)
+# #             for j in range(0,len(nums2)):
+# #                 if nums2[j] == nums1[i]:
+# #                     for k in range(j,len(nums2)):
+# #                         if nums2[k] > nums2[j]:
+# #                             result.append(nums2[k])
+# #                             find = False
+# #                             break
+# #                     if find is True:
+# #                         result.append(-1)
                     
         return result
