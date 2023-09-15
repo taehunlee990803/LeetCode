b37047e3fb8b4c42aa6bb316700872e516128f57
Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2023-09-14 20:37:05 -0400

    Time: 53 ms (30.26%), Space: 13.3 MB (96.99%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -8,16 +8,40 @@ class Solution(object):
         result = []
         find = True
         
+         
+        # for i in range(0,len(nums1)):
+        #     find = True
+        #     for j in range(0,len(nums2)):
+        #         if nums2[j] == nums1[i]:
+        #             for k in range(j,len(nums2)):
+        #                 if nums2[k] > nums2[j]:
+        #                     result.append(nums2[k])
+        #                     find = False
+        #                     break
+        #             if find is True:
+        #                 result.append(-1)
+        temp_index = 0
         for i in range(0,len(nums1)):
             find = True
-            for j in range(0,len(nums2)):
-                if nums2[j] == nums1[i]:
-                    for k in range(j,len(nums2)):
-                        if nums2[k] > nums2[j]:
-                            result.append(nums2[k])
-                            find = False
-                            break
-                    if find is True:
-                        result.append(-1)
+            temp_index = nums2.index(nums1[i])
+            
+            for j in range(temp_index, len(nums2)):
+                if nums2[j] > nums1[i]:
+                    result.append(nums2[j])
+                    find = False
+                    break
+            if find is True:
+                result.append(-1)
+                    
+            
+#             for j in range(0,len(nums2)):
+#                 if nums2[j] == nums1[i]:
+#                     for k in range(j,len(nums2)):
+#                         if nums2[k] > nums2[j]:
+#                             result.append(nums2[k])
+#                             find = False
+#                             break
+#                     if find is True:
+#                         result.append(-1)
                     
         return result
