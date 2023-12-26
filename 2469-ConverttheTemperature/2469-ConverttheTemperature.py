c5bf5862ff8d7ad289a78cceb6623a5fb7c6ccb1
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2023-12-26 18:29:43 -0500

    Update file 12/26/2023, 6:29:43 PM

--- 2469-ConverttheTemperature/2469-ConverttheTemperature.py
+++ 2469-ConverttheTemperature/2469-ConverttheTemperature.py
@@ -1,9 +1,5 @@
 class Solution:
     def convertTemperature(self, celsius: float) -> List[float]:
-        ans = []
-        kelvin = round(celsius + 273.15, 5)        
-        fehr = round(celsius*1.80 + 32.00, 5)
-        ans.append(kelvin)
-        ans.append(fehr)
-        return ans
+   
+        return [celsius + 273.15, celsius*1.80 + 32.00]
 3
