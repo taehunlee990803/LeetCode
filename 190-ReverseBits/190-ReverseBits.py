22e0b45b54cfa94206921f2ee73304e6c6701e48
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2023-12-26 18:00:04 -0500

    Update file 12/26/2023, 6:00:04 PM

--- 190-ReverseBits/190-ReverseBits.py
+++ 190-ReverseBits/190-ReverseBits.py
@@ -1,6 +1,12 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        num = bin(n)[2:].zfill(32)
-        return int(num[::-1], 2)
+        ans = 0
+        i = 0
 
+            if num[x] == '1':
+                ans += pow(2,i)
+            i += 1
+        num = bin(n)[2:].zfill(32)
+        for x in range(0,len(num)):
+        return ans 
 0
