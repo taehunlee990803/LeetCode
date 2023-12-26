de203536b8a71905ee9e44f3e3533a70d3d49899
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2023-12-26 16:12:23 -0500

    Create file

---
+++ 1155-NumberofDiceRollsWithTargetSum/1155-NumberofDiceRollsWithTargetSum.py
@@ -0,0 +1,27 @@
+                    # else:
+                    #     new_dict[temp] += 1
+                    #     new_dict[temp] = 1
+                    # if temp in new_dict:
+                    # temp = element + i
+                    new_dict[temp] = (new_dict.get(temp, 0) + dict_sum[element]) % output
+                    #  for i in range(1, k+1):
+                    temp = element + i
+                for i in range(1,k+1):
+            new_dict = {}
+            for element in dict_sum:
+        output = 10**9 + 7
+        for count in range(0,n):
+        dict_sum[0] = 1
+        
+        # for i in range(1,k+1):
+        #     dict_sum[i] = 1
+            dict_sum = new_dict 
+        
+        if target in dict_sum:
+        # return dict_sum[target] % output
+        # dict_sum = {}
+        # dict_sum[0] = 1  # Initialize with 1 way to get a sum of 0
+            return dict_sum[target] % output
+        return 0
+
+2
