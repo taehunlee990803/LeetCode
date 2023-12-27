f54d37b55dbc8da9b74d6ef0d769dfeb005dcee9
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2023-12-27 01:10:11 -0500

    Update file 12/27/2023, 1:10:10 AM

--- 49-GroupAnagrams/49-GroupAnagrams.py
+++ 49-GroupAnagrams/49-GroupAnagrams.py
@@ -1,22 +1,22 @@
+        # dict_container = {}
+        # index = 0
+        # for element in strs:
+        #     temp = str(element)
+        #     temp_sorted = ''.join(sorted(temp))
+        #     if temp_sorted not in dict_container:
+        #         dict_container[temp_sorted] = index
+        #         arr.append([element])
+        #         index += 1
+        #     else:
+        #         k = dict_container[temp_sorted]
+        # arr = []
+        anagram_map = defaultdict(list)
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-
-        arr = []
-        dict_container = {}
-        index = 0
-        for element in strs:
-            temp = str(element)
-            temp_sorted = ''.join(sorted(temp))
-            if temp_sorted not in dict_container:
-                dict_container[temp_sorted] = index
-                arr.append([element])
-                index += 1
-            else:
-                k = dict_container[temp_sorted]
-                arr[k].append(element)
-
-
-          
-                # t = len(arr[k])
-        return arr
-[
+        for word in strs:
+            sorted_word = ''.join(sorted(word))
+            anagram_map[sorted_word].append(word)
+        return list(anagram_map.values())
+["eat","tea","tan","ate","nat","bat"]
+[""]
+["a"]
