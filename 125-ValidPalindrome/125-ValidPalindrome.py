b56c0a5ecf788db3e6b15c524ce9883d65bb704e
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2023-12-27 16:43:12 -0500

    Update file 12/27/2023, 4:43:12 PM

--- 125-ValidPalindrome/125-ValidPalindrome.py
+++ 125-ValidPalindrome/125-ValidPalindrome.py
@@ -1,16 +1,14 @@
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        # convert_letter = ""
-        # for element in s:
-        #     if element.isalpha() or element.isnumeric():
-        #         convert_letter += element.lower()
-        # reverse_letter = convert_letter[::-1]
+            i = i + (not a.isalnum())
+                    continue
+                    j = j - 1
+                    i = i + 1
+                else:
+                if a!=b: return False
+            if a.isalnum() and b.isalnum():
 
-        # print(reverse_letter)
-        # print(convert_letter)
-        # if len(s) == 1:
-        #     return True
-        s = [i for i in s.lower() if i.isalnum()]
-        return s == s[::-1]
-        # return reverse_letter == convert_letter
+            b = s[j].lower()
+            a = s[i].lower()
+            j = j - (not b.isalnum())
+        return True
+
 "
