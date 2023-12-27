a301d69a424096fe8c5a0d657e209f81a821809c
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2023-12-27 17:06:43 -0500

    Update file 12/27/2023, 5:06:43 PM

--- 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.py
+++ 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.py
@@ -1,18 +1,17 @@
-        l = 0
-        # while l != len(numbers) -1:
-        #     r = l + 1
-        #     while r != len(numbers):
-        #         if numbers[l] + numbers[r] > target:
-        #             r = len(numbers) -1
-        #         if numbers[l] + numbers[r] == target:
-        r = len(numbers) -1
-        while l != r:
-            if numbers[l] + numbers[r] == target:
-                return [l+1, r+1]
-
-            elif numbers[l] + numbers[r] > target:
-                r -= 1
-            else:
-                l += 1
+        # while l != r:
+        #     if numbers[l] + numbers[r] == target:
+        #         return [l+1, r+1]
+        # r = len(numbers) -1
+        # l = 0
+        # ans = []
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+class Solution:
+        dic = {}
+        for i, num in enumerate(numbers):
+            temp = target - num
+        return [1,1]
+            if temp in dic:
+                return [dic[target-num]+1, i+1]
+            dic[num] = i
 [
 0
