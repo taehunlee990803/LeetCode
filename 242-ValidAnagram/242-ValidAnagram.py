6f646090cb0fcafb23ba244ce9f0c820e3eddd17
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2023-12-27 00:20:08 -0500

    Create file

---
+++ 242-ValidAnagram/242-ValidAnagram.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        dict_s = Counter(s)
+        dict_t = Counter(t)
+
+        return dict_s == dict_t
+"anagram"
+"nagaram"
+"rat"
+"car"
