2d6ac5865ff73970f8378850e3f80e6c09d14ddf
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2023-12-27 18:19:56 -0500

    Update file 12/27/2023, 6:19:56 PM

--- 74-Searcha2DMatrix/74-Searcha2DMatrix.py
+++ 74-Searcha2DMatrix/74-Searcha2DMatrix.py
@@ -1,13 +1,17 @@
-                    m = (l+r)//2
-                    if element[m] == target:
-                        return True
-                    elif element[m] > target:
-                        r = m - 1
-                    else:
-                        l = m + 1
-
-           
-
-        return False
-                while l<=r:
+        while l<=r:
+            m = (l+r)//2
+            if list1[m] == target:
+                return True
+            elif list1[m] > target:
+                r = m - 1
+        r = len(list1) -1
+        l = 0
+                break
+                list1 = row
+            if row[-1] >= target:
+        for row in matrix:
+        prev = 0
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+class Solution:
+        list1 = []
 [
