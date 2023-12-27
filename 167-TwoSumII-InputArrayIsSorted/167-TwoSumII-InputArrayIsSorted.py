903c33b9cfe9b9a4b10b9aec43f20ece37a53b47
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2023-12-27 17:01:42 -0500

    Create file

---
+++ 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.py
@@ -0,0 +1,18 @@
+        l = 0
+        # while l != len(numbers) -1:
+        #     r = l + 1
+        #     while r != len(numbers):
+        #         if numbers[l] + numbers[r] > target:
+        #             r = len(numbers) -1
+        #         if numbers[l] + numbers[r] == target:
+        r = len(numbers) -1
+        while l != r:
+            if numbers[l] + numbers[r] == target:
+                return [l+1, r+1]
+
+            elif numbers[l] + numbers[r] > target:
+                r -= 1
+            else:
+                l += 1
+[
+0
