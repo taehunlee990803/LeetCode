d9564a8ecf4085877890fb39e4dff993fbfd6c34
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2023-12-28 01:09:20 -0500

    Update file 12/28/2023, 1:09:19 AM

--- 739-DailyTemperatures/739-DailyTemperatures.py
+++ 739-DailyTemperatures/739-DailyTemperatures.py
@@ -1,25 +1,25 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        stack = [0] * len(temperatures)
-        count = 0
-        length = len(temperatures)
-        for i in range(0,length):
-            current = temperatures[i]
-            for j in range(i+1, length):
-                if temperatures[j] > current:
-                    break
-            # count = 0
-        return stack
+        # stack = [0] * len(temperatures)
+        # count = 0
+        # length = len(temperatures)
+        # for i in range(0,length):
+        #     current = temperatures[i]
+        #     for j in range(i+1, length):
+        #         if temperatures[j] > current:
+        #             break
+        #     # count = 0
+        # return stack
         
-                    stack[i] = j - i
-        # result = [0]*len(temperatures)
-        # stack = []
-        # for index, temp in enumerate(temperatures):
-        #         prev_temp = stack.pop()
-        #     while stack and temperatures[stack[-1]] < temp:
-        #         result[prev_temp] = index - prev_temp
-        #     stack.append(index)
-        # return result
+        #             stack[i] = j - i
+········result·=·[0]*len(temperatures)
+········stack·=·[]
+········for·index,·temp·in·enumerate(temperatures):
+················prev_temp·=·stack.pop()
+············while·stack·and·temperatures[stack[-1]]·<·temp:
+················result[prev_temp]·=·index·-·prev_temp
+············stack.append(index)
+········return·result
 
 
 
