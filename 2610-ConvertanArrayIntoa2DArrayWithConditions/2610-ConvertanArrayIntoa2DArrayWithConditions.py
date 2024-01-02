f7a24c32645c21420357e8f7cc77594c39784442
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2024-01-01 23:25:39 -0500

    Create file

---
+++ 2610-ConvertanArrayIntoa2DArrayWithConditions/2610-ConvertanArrayIntoa2DArrayWithConditions.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findMatrix(self, nums: List[int]) -> List[List[int]]:
+        return container
+          
+
+
+        dic = Counter(nums)
+        if len(dic) == len(nums):
+            return [nums]
+        for element in dic:
+            count = dic[element]
+        container = []
+            if len(container) < count:
+            else:
+                for i in range(0,count):
+                    container[i].append(element)
+                for i in range(len(container)):
+                    container[i].append(element)
+                dif = count - len(container)
+                for j in range(dif):
+                    temp = [element]
+                    container.append(temp)
+        
+[
