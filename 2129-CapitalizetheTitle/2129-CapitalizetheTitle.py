7e218e48cab2e413ad02a8cacae5640f7a18f99e
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2024-01-01 22:58:11 -0500

    Update file 1/1/2024, 10:58:11 PM

--- 2129-CapitalizetheTitle/2129-CapitalizetheTitle.py
+++ 2129-CapitalizetheTitle/2129-CapitalizetheTitle.py
@@ -1,12 +1,26 @@
-class Solution:
-    def capitalizeTitle(self, title: str) -> str:
+        #     return ans
+        # else:
+        temp = ""
+        for element in title:
+            if element == " ":
+        container = []
+                container.append(temp)
         ans = ""
-        if len(title) <= 2:
-            for element in title:
-                if ord(element) >= 65 and ord(element)<=90:
-                    ans += chr(ord(element)+32)
-                else:
-                    ans+= chr(element)
-            return ans
-        
+                temp = ""
+            else:
+                temp += element
+        for i in range(len(container)):
+            if len(container[i]) > 2:
+                for j in range(len(container[i])):
+                    if j == 0:
+                        ans += container[i][0].upper()
+                    else:
+                        ans += container[i][j].lower()
+            else:
+                for j in range(len(container[i])):
+                    ans += container[i][j].lower()
+            if i is not len(container) - 1:
+                ans += " "
+        container.append(temp)
+        #             ans+= chr(element)
 "
