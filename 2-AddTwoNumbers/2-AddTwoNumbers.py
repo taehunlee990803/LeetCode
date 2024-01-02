6dfdaa78f36d22dc4638d90605bd9187bc53b091
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2024-01-02 01:54:21 -0500

    Create file

---
+++ 2-AddTwoNumbers/2-AddTwoNumbers.py
@@ -0,0 +1,26 @@
+        # else:
+        #     temp = 0
+        #     for i in range(len(container2)-1,-1,-1):
+        #         sum1 += container1[i]*pow(10,temp)
+        #         temp+=1
+        #     temp = len(container2)-1
+        #     for i in range(0,len(container1)):
+        #         sum2 += container2[i]*pow(10,temp)
+        #         temp-=1
+
+
+
+
+        # for i in range(len(container1)-1,-1,-1):
+        #     sum1 += container1[i]*pow(10,temp)
+        #     temp+=1
+        # temp = 0
+        # for i in range(len(container2)-1,-1,-1):
+        #     sum2 += container2[i]*pow(10,temp)
+        #     temp+=1
+
+
+
+        sum_total = sum1 + sum2
+        print(sum_total)
+[
