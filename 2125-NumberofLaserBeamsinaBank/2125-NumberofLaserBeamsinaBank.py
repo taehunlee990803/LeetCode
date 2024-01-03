fda0006f3912932f57d00d4b52f199cc75aae71c
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2024-01-02 22:25:19 -0500

    Update file 1/2/2024, 10:25:19 PM

--- 2125-NumberofLaserBeamsinaBank/2125-NumberofLaserBeamsinaBank.py
+++ 2125-NumberofLaserBeamsinaBank/2125-NumberofLaserBeamsinaBank.py
@@ -1,26 +1,30 @@
-        #             temp+=1
-            
-        
         # for i in range(0,len(container)-1):
         #     count += container[i]*container[i+1]
-        #     if temp != 0:
-        #         container.append(temp)
         # return count
-        count = 0
-        for i in range(0,height):
-        temp = 0
-            for j in range(0,width):
-                if int(bank[i][j]) == 1:
-            temp = 0
-                    temp+=1
-            if zero == 0:
-        zero = 0
-                zero = temp
-            else:
-                if temp != 0:
-                    count += zero*temp
-                    zero = temp
-        return count 
-                    print(zero, temp)
+        # count = 0
+        # for i in range(0,height):
+        # temp = 0
+        #     for j in range(0,width):
+        #         if int(bank[i][j]) == 1:
+        #     temp = 0
+        #             temp+=1
+        #     if zero == 0:
+        # zero = 0
+        #         zero = temp
+        #     else:
+        #         if temp != 0:
+        #             count += zero*temp
+        #             zero = temp
+        # return count 
+        #             print(zero, temp)
+
+
+        ans = prev = 0
+        for s in bank:
+            c = s.count('1')
+            if c:
+                ans += prev*c
+                prev = c
+        return ans 
             
 ["011001","000000","010100","001000"]
