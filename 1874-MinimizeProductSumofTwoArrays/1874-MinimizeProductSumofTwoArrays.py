9c5353cc89ba207cf2c824a361dec6d0e20b428a
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2024-01-02 23:22:50 -0500

    Create file

---
+++ 1874-MinimizeProductSumofTwoArrays/1874-MinimizeProductSumofTwoArrays.py
@@ -0,0 +1,30 @@
+        # for i in range(0,len(nums1)-1):
+        #     if (nums2[i] > nums2[i+1]):
+        #         if nums1[i] > nums1[i+1]:
+        #             temp = nums1[i]
+        #             nums1[i] = nums1[i+1]
+        #             nums1[i+1] = temp
+        #     elif nums2[i] <= nums2[i+1]:
+        #         if nums1[i] < nums1[i+1]:
+        #             temp = nums1[i]
+        #             nums1[i] = nums1[i+1]
+        #             nums1[i+1] = temp
+        # ans = 0
+        # for i in range(0,len(nums1)):
+
+        # dic = {}
+        for i in range(length):
+        length = len(temp1)
+        # print(dic)
+        ans= 0
+    
+            ans += temp1[i]*temp2[length-1-i]
+        temp2.sort()
+        temp1.sort()
+        temp1 = nums1
+        temp2 = nums2
+class Solution:
+    def minProductSum(self, nums1: List[int], nums2: List[int]) -> int:
+
+        #     ans += nums1[i]*nums2[i]
+[5,3,4,2]
