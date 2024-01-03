4015b9af9f1b257dee4672212a5c3ed8b7a37a30
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2024-01-02 22:24:21 -0500

    Update file 1/2/2024, 10:24:20 PM

--- 2125-NumberofLaserBeamsinaBank/2125-NumberofLaserBeamsinaBank.py
+++ 2125-NumberofLaserBeamsinaBank/2125-NumberofLaserBeamsinaBank.py
@@ -1,20 +1,26 @@
-class Solution:
-    def numberOfBeams(self, bank: List[str]) -> int:
-        count = 0
-        height = len(bank)
-        container = []
-        width = len(bank[0])
-        for i in range(0,height):
-            temp = 0
-            for j in range(0,width):
-                if int(bank[i][j]) == 1:
-                    temp+=1
+        #             temp+=1
             
         
-        for i in range(0,len(container)-1):
-            count += container[i]*container[i+1]
-            if temp != 0:
-                container.append(temp)
-        return count
-
+        # for i in range(0,len(container)-1):
+        #     count += container[i]*container[i+1]
+        #     if temp != 0:
+        #         container.append(temp)
+        # return count
+        count = 0
+        for i in range(0,height):
+        temp = 0
+            for j in range(0,width):
+                if int(bank[i][j]) == 1:
+            temp = 0
+                    temp+=1
+            if zero == 0:
+        zero = 0
+                zero = temp
+            else:
+                if temp != 0:
+                    count += zero*temp
+                    zero = temp
+        return count 
+                    print(zero, temp)
+            
 ["011001","000000","010100","001000"]
