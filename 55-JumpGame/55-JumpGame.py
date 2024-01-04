42c0cdd6812a2e403aa6817ab0711bbafe0b093b
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2024-01-04 16:51:58 -0500

    Create file

---
+++ 55-JumpGame/55-JumpGame.py
@@ -0,0 +1,12 @@
+        max_distance = 0
+        while loc <= max_distance:  # Change the condition to handle the case where loc exceeds max_distance
+                return True
+
+            max_distance = max(max_distance, temp)
+            loc += 1
+
+            temp = loc + nums[loc]
+
+            if temp >= destination:
+        return False
+[2,3,1,1,4]
