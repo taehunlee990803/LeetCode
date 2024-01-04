6ea91e62e506e61fafdffe0346cdcb11b06a306f
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2024-01-03 23:23:34 -0500

    Create file

---
+++ 2870-MinimumNumberofOperationstoMakeArrayEmpty/2870-MinimumNumberofOperationstoMakeArrayEmpty.py
@@ -0,0 +1,18 @@
+            temp = dic[element]
+            if temp%3 == 0:
+                count += temp//3
+            else:
+        for element in dic:
+
+                dic[element] = 1
+            print(temp)
+            elif temp%3 == 1 or temp%3 == 2:
+                count += temp //3 + 1
+        return count 
+
+      
+                return -1
+       
+            if temp == 1:
+                return -1
+[2,3,3,2,2,4,2,3,4]
