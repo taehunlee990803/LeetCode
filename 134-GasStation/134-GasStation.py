9fc527294d64af625d3d8bdc0ca0c53e441307f2
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2024-01-04 18:32:08 -0500

    Create file

---
+++ 134-GasStation/134-GasStation.py
@@ -0,0 +1,20 @@
+          
+        #     for j in range(0,length-1): #12345
+        #         next_idx = i + j
+        #         next_next_idx = i + j + 1
+        #         if next_idx >= length: 
+        #             next_idx -= length
+        #         if next_next_idx >= length: 
+        #         minimum_gas = cost[i-1]
+        #     temp = True
+        #     if i - 1 < 0:
+        #         minimum_gas = cost[length - 1]
+        #     else:
+        # for i in range(length):#01234
+        #     current = gas[i]
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        # length = len(gas)
+        # ans = -1
+class Solution:
+[1,2,3,4,5]
+[
