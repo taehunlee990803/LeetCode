8f67aac0b569f8371952d06e47873655c5eeb725
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2024-01-04 16:11:57 -0500

    Create file

---
+++ 1323-Maximum69Number/1323-Maximum69Number.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maximum69Number (self, num: int) -> int:
+        temp = str(num)
+       
+        ans = ""
+        
+        for i in range(len(temp)):
+            if temp[i] == '6':
+        return int(ans)
+                ans += "9"
+       
+                ans += temp[i+1:len(temp)]
+            ans += "9"
+                return int(ans)
+        # return int(temp)
+9669
