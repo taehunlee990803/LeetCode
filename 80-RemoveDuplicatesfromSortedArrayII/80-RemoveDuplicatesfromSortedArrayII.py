3d1f46cd8d4258406c1cfc66ef172e24c61b9d9f
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2024-01-06 01:14:10 -0500

    Create file

---
+++ 80-RemoveDuplicatesfromSortedArrayII/80-RemoveDuplicatesfromSortedArrayII.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        
+        k = 0
+        for i in nums:
+            if k < 2 or i!=nums[k-2]:
+                nums[k] = i
+                k += 1
+
+        return k
+
+[1,1,1,2,2,3]
