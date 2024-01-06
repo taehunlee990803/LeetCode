3c61f7a35bb2e026513d5ea43ab52d27a75155fa
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2024-01-06 01:16:30 -0500

    Update file 1/6/2024, 1:16:30 AM

--- 80-RemoveDuplicatesfromSortedArrayII/80-RemoveDuplicatesfromSortedArrayII.py
+++ 80-RemoveDuplicatesfromSortedArrayII/80-RemoveDuplicatesfromSortedArrayII.py
@@ -1,12 +1,15 @@
-class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
-        
-        k = 0
-        for i in nums:
-            if k < 2 or i!=nums[k-2]:
-                nums[k] = i
-                k += 1
+        p1 = 1
+        p2 = 2
 
-        return k
-
+        while p2 < len(nums):
+            if nums[p1] == nums[p1-1] and nums[p2] == nums[p2-1] == nums[p2-2]:
+                while p2 < len(nums) and nums[p2] == nums[p2-1]:
+                    p2+=1
+                if p2 == len(nums):
+                    break
+            p1+=1
+            nums[p1] = nums[p2]
+            p2+=1
+        return p1+1
+        # k = 0
 [1,1,1,2,2,3]
