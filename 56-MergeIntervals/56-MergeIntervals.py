e149b095e1fa100723b5dfc9c78b4f09fd48e510
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2024-01-09 20:28:48 -0500

    Update file 1/9/2024, 8:28:47 PM

--- 56-MergeIntervals/56-MergeIntervals.py
+++ 56-MergeIntervals/56-MergeIntervals.py
@@ -1,30 +1,31 @@
-        # while i < len(intervals):
+            #         r1 = intervals[i][0]
+            #         l2 = ans[j][1]
+            #         l1= ans[j][0]
+            #     if temp <= ans[j][1]:
+            # for j in range(len(ans)):
+                l2 = ans[-1][1]
+                r1 = intervals[i][0]
+                r2 = intervals[i][1]
+                low = min(l1,r1)
+                high = max(l2, r2)
+                ans[-1] = [low,high]
+                # check = True
+                # break
 
-        return ans
-                    break
-                    ans[j] = [low,high]
-                    high = max(l2, r2)
-                    low = min(l1,r1)
-                    r2 = intervals[i][1]
-                    r1 = intervals[i][0]
-                    l2 = ans[j][1]
-                    l1= ans[j][0]
-                if temp <= ans[j][1]:
-            for j in range(len(ans)):
+
+            else:
+                ans.append(intervals[i])
+            #         r2 = intervals[i][1]
+                l1= ans[-1][0]
+            if ans[-1][-1] >= intervals[i][0]:
+            check = False
             temp = intervals[i][0]
+        
         for i in range(1, len(intervals)):
         ans = [intervals[0]]
-        i = 0
         intervals.sort()
+        i = 0
       
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
 class Solution:
-               
-                    
-        
-            check = False
-                    check = True
-            if check == False:
-
-                ans.append(intervals[i])
 [[1,3],[2,6],[8,10],[15,18]]
