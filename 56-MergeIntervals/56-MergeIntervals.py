07084648f04dc4c2413409b350584c7d741f41ee
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2024-01-09 20:26:49 -0500

    Create file

---
+++ 56-MergeIntervals/56-MergeIntervals.py
@@ -0,0 +1,30 @@
+        # while i < len(intervals):
+
+        return ans
+                    break
+                    ans[j] = [low,high]
+                    high = max(l2, r2)
+                    low = min(l1,r1)
+                    r2 = intervals[i][1]
+                    r1 = intervals[i][0]
+                    l2 = ans[j][1]
+                    l1= ans[j][0]
+                if temp <= ans[j][1]:
+            for j in range(len(ans)):
+            temp = intervals[i][0]
+        for i in range(1, len(intervals)):
+        ans = [intervals[0]]
+        i = 0
+        intervals.sort()
+      
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+class Solution:
+               
+                    
+        
+            check = False
+                    check = True
+            if check == False:
+
+                ans.append(intervals[i])
+[[1,3],[2,6],[8,10],[15,18]]
