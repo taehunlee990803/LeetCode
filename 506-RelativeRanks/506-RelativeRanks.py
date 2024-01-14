2c5bdfb17da94a1c9cc6ec1be92d53bedba2d041
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2024-01-14 17:27:24 -0500

    Create file

---
+++ 506-RelativeRanks/506-RelativeRanks.py
@@ -0,0 +1,17 @@
+        ans = []
+
+                    container.append(j+1)
+                if temp[j] == score[i]:
+                    break
+       
+            for j in range(len(temp)):
+        for i in range(len(score)):
+        temp.sort(reverse=True)
+        container = []
+        temp = score.copy()
+        for i in range(len(container)):
+            if container[i] == 1:
+                ans.append("Gold Medal")
+            elif container[i] == 2:
+                ans.append("Silver Medal")
+[
