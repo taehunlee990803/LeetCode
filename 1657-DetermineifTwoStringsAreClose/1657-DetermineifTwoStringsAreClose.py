fa04cc337525f92b8361fc5b53ff87ab1d6e7a66
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2024-01-14 18:56:21 -0500

    Create file

---
+++ 1657-DetermineifTwoStringsAreClose/1657-DetermineifTwoStringsAreClose.py
@@ -0,0 +1,21 @@
+        for ch in word1:
+            freq1[ord(ch) - ord('a')] += 1
+
+        for ch in word2:
+            freq2[ord(ch) - ord('a')] += 1
+
+        for i in range(26):
+            if (freq1[i] == 0 and freq2[i] != 0) or (freq1[i] != 0 and freq2[i] == 0):
+                return False
+
+        freq1.sort()
+        freq2.sort()
+
+        for i in range(26):
+
+        freq2 = [0] * 26
+        freq1 = [0] * 26
+    def closeStrings(self, word1: str, word2: str) -> bool:
+class Solution:
+
+"
