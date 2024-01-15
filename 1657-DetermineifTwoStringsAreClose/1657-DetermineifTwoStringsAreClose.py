860774776ea3f7ba3004299b6f0bb03d90c27ae0
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2024-01-15 17:08:21 -0500

    Update file 1/15/2024, 5:08:21 PM

--- 1657-DetermineifTwoStringsAreClose/1657-DetermineifTwoStringsAreClose.py
+++ 1657-DetermineifTwoStringsAreClose/1657-DetermineifTwoStringsAreClose.py
@@ -1,21 +1,16 @@
-        for ch in word1:
-            freq1[ord(ch) - ord('a')] += 1
-
-        for ch in word2:
-            freq2[ord(ch) - ord('a')] += 1
-
-        for i in range(26):
-            if (freq1[i] == 0 and freq2[i] != 0) or (freq1[i] != 0 and freq2[i] == 0):
-                return False
-
-        freq1.sort()
-        freq2.sort()
-
-        for i in range(26):
-
-        freq2 = [0] * 26
-        freq1 = [0] * 26
-    def closeStrings(self, word1: str, word2: str) -> bool:
 class Solution:
+    def calculateTime(self, keyboard: str, word: str) -> int:
+        dic = {}
 
+        for i in range(26):
+            dic[keyboard[i]] = i
+        
+        # time = dic[word[0]]
+        ans = dic[word[0]]
+        # for i in range()
+        for i in range(len(word)-1):
+            first = dic[word[i]]
+            second = dic[word[i+1]]
+            ans += abs(first - second)
+        return ans 
 "
