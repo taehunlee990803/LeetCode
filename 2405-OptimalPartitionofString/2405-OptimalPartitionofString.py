eaa4a217855b2f71b4509add94748422d97a36dd
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2024-01-15 14:33:05 -0500

    Create file

---
+++ 2405-OptimalPartitionofString/2405-OptimalPartitionofString.py
@@ -0,0 +1,23 @@
+        # return ans 
+        #     ans = max(dic[element], ans)
+        container = [[]]
+        for element in s:
+            if element in container[index]:
+            check = False
+        index = 0
+                index += 1
+            else:
+                container.append([element])
+        # for element in dic:
+        # ans = 0
+        #     else:
+        #         dic[element] = 1
+        # for element in s:
+        #     if element in dic:
+        #         dic[element] += 1
+        # dic = {}
+        # container = []
+        # temp_container = []
+class Solution:
+    def partitionString(self, s: str) -> int:
+"
