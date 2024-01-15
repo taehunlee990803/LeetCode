52e70138d5478677235e063e0072ce2918fd7946
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2024-01-15 02:01:21 -0500

    Create file

---
+++ 2225-FindPlayersWithZeroorOneLosses/2225-FindPlayersWithZeroorOneLosses.py
@@ -0,0 +1,13 @@
+        zero = sorted([k for k, l in lost.items() if l == 0])
+        ones = sorted([k for k, l in lost.items() if l == 1])
+        
+        return [zero, ones]
+        
+            lost[m[1]] += 1
+            lost[m[0]] += 0
+        for m in matches:
+        
+        lost = defaultdict(int)
+    def findWinners(self, matches):
+class Solution:
+[
