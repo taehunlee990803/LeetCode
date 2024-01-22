217c5523530398d43b2d4aba13adfe033da42a0c
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2024-01-21 19:36:06 -0500

    Update file 1/21/2024, 7:36:05 PM

--- 1167-MinimumCosttoConnectSticks/1167-MinimumCosttoConnectSticks.py
+++ 1167-MinimumCosttoConnectSticks/1167-MinimumCosttoConnectSticks.py
@@ -1,19 +1,24 @@
 class Solution:
     def connectSticks(self, sticks: List[int]) -> int:
-        if len(sticks) == 1:
-            return 0
+        # if len(sticks) == 1:
+        #     return 0
 
-        cost = 0
-        while len(sticks) >= 2:
-            sticks.sort()
-
-            cost += temp
-            sticks.remove(sticks[0])
-            sticks.remove(sticks[0])
-        return cost 
-
+        # cost = 0
+        # while len(sticks) >= 2:
+        #     sticks.sort()
+        #     temp = (sticks[0] + sticks[1])
+        #     cost += temp
+        #     sticks.remove(sticks[0])
+        #     sticks.remove(sticks[0])
+        #     sticks.append(temp)
             
-            temp = (sticks[0] + sticks[1])
-            sticks.append(temp)
 
+        heapq.heapify(sticks)
+        sum = 0 
+        while len(sticks) >= 2:
+            first = heapq.heappop(sticks)
+            second = heapq.heappop(sticks)
+            sum += first + second
+            heapq.heappush(sticks, first+second)
+        return sum
 [
