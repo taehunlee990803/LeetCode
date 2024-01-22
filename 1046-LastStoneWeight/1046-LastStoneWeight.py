637f66f9091e81ca05fa11c6fa959e280e56980a
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2024-01-21 19:53:42 -0500

    Update file 1/21/2024, 7:53:42 PM

--- 1046-LastStoneWeight/1046-LastStoneWeight.py
+++ 1046-LastStoneWeight/1046-LastStoneWeight.py
@@ -1,24 +1,24 @@
-class Solution:
-    def lastStoneWeight(self, stones: List[int]) -> int:
-        
-        # heapq.heapifty(stones)
-
-        while len(stones) >= 2:
-            stones.sort()
-            big = stones[-1]
-        if len(stones) == 1:
-            small = stones[-2]
-            if big == small:
-                stones.remove(big)
-                stones.remove(small)
+            # dic[items[i][0]].append(items[i][1])
+            # dic[id].append(score)
             else:
-                stones.remove(small)
-                stones.append(big-small)
-            return stones[0]
-        else:
-            return 0
-            # print(big, small)
-            # stones.remove(big)
-                stones.remove(big)
-        # return stones[0]
+                dic[id] = [score]
+        ans = []
+        for element in dic:
+            for score in dic[element]:
+            sum = 0
+                container.append(score)
+            score = items[i][1]
+            if id in dic:
+                dic[id].append(score)
+            id = items[i][0]
+        for i in range(len(items)):
+        print(dic)
+            container = []
+            container.sort()
+            sum += (container[-1] + container[-2] + container[-3] + container[-4] + container[-5])
+            temp = [element, int(sum/5)]
+        dic = {}
+class Solution:
+    def highFive(self, items: List[List[int]]) -> List[List[int]]:
+        items.sort()
 [
