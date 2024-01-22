f0b06d376c7859e599adcd199dc1947bb54c018b
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2024-01-22 10:54:00 -0500

    Create file

---
+++ 1704-DetermineifStringHalvesAreAlike/1704-DetermineifStringHalvesAreAlike.py
@@ -0,0 +1,25 @@
+class Solution:
+    def halvesAreAlike(self, s: str) -> bool:
+        lower = ['a','e','i','o','u']
+        upper = ['A','E','I','O','U']
+        countFirst = 0
+        countSecond = 0
+        dic_first = Counter(s[:length//2])
+        dic_second = Counter(s[length//2:])
+        for element in dic_first:
+            if element in lower:
+                countFirst += dic_first[element]
+            if element in upper:
+                countFirst += dic_first[element]
+        length = len(s)
+        for element in dic_second:
+            if element in lower:
+                countSecond += dic_second[element]
+            if element in upper:
+                countSecond+= dic_second[element]
+
+        
+        return countFirst == countSecond
+
+        
+"
