54656d7bd4335c6bab88375e77e79cd8b2b1626b
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2024-01-24 18:21:04 -0500

    Update file 1/24/2024, 6:21:04 PM

--- 452-MinimumNumberofArrowstoBurstBalloons/452-MinimumNumberofArrowstoBurstBalloons.py
+++ 452-MinimumNumberofArrowstoBurstBalloons/452-MinimumNumberofArrowstoBurstBalloons.py
@@ -1,26 +1,10 @@
 class Solution:
     def findMinArrowShots(self, points: List[List[int]]) -> int:
-        # ans = 0
-        # points.sort()
-
-        # total = len(points)
-        # temp = 0
-        # first = points[temp][1]
-
-        #     # print(points[i][0], points[i][1], first,ans)
-        #     # if points[i][0] > first or points[i][1] < first:
-        #     #     ans += 1
-        #     #     if i != len(points)-1:
-        #     if points[i][0] <= first and points[i][1] >= first:
-        #         ans += 1
-        #     else:
-        #         first = points[i][1]
-        # for i in range(1, len(points)):
         if not points:
             return 0
 
         points.sort(key=lambda x: x[1])  # Sort by end points
-
+        print(points)
         ans = 1
         current_end = points[0][1]
 
@@ -31,6 +15,6 @@ class Solution:
                 current_end = end
 
         return ans
-        #     #         first = points[i+1][1]
-        #     #     else:
-[
+        # ans = 0
+        # points.sort()
+[[10,16],[2,8],[1,6],[7,12]]
