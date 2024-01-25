a537a8eda3690e5c9247ea9c6953d810becc45ff
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2024-01-25 10:33:28 -0500

    Update file 1/25/2024, 10:33:28 AM

--- 2486-AppendCharacterstoStringtoMakeSubsequence/2486-AppendCharacterstoStringtoMakeSubsequence.py
+++ 2486-AppendCharacterstoStringtoMakeSubsequence/2486-AppendCharacterstoStringtoMakeSubsequence.py
@@ -8,11 +8,11 @@ class Solution:
             if element == t[temp] and temp < len(t):
                 temp+=1
 
-        if temp < len(t):
-        else:
-            return len(t) - temp
-            return 0
+        # if temp < len(t):
+        # else:
+        #     return len(t) - temp
+        #     return 0
                 if temp == len(t):
                     return 0
-        # return len(t) - temp
+        return len(t) - temp
 "
