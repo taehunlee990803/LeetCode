6170987fc7ba9979a48788fc4e709256ab8b92b5
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2024-02-01 17:15:31 -0500

    Create file

---
+++ 2966-DivideArrayIntoArraysWithMaxDifference/2966-DivideArrayIntoArraysWithMaxDifference.py
@@ -0,0 +1,18 @@
+                temp.append(element)
+            else:
+                if len(temp) < 3:
+                        temp.append(element)
+                else:
+                    ans.append(temp)
+                    temp = [element]
+                    if element - temp[0] <= k:
+                    else:
+                        return []
+            if len(temp) == 0:
+        for element in nums:
+        temp = []
+
+        if len(temp) == 3:
+            ans.append(temp)
+
+[
