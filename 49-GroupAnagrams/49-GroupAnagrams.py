07ae52929b48b83ccbc7de021befeac2e844b668
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2024-02-06 17:19:57 -0500

    Update file 2/6/2024, 5:19:56 PM

--- 49-GroupAnagrams/49-GroupAnagrams.py
+++ 49-GroupAnagrams/49-GroupAnagrams.py
@@ -1,3 +1,11 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagram_map = defaultdict(list)
+        for word in strs:
+            sorted_word = ''.join(sorted(word))
+            anagram_map[sorted_word].append(word)
+        return list(anagram_map.values())
+        # arr = []
         # dict_container = {}
         # index = 0
         # for element in strs:
@@ -7,16 +15,4 @@
         #         dict_container[temp_sorted] = index
         #         arr.append([element])
         #         index += 1
-        #     else:
-        #         k = dict_container[temp_sorted]
-        # arr = []
-        anagram_map = defaultdict(list)
-class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        for word in strs:
-            sorted_word = ''.join(sorted(word))
-            anagram_map[sorted_word].append(word)
-        return list(anagram_map.values())
-["eat","tea","tan","ate","nat","bat"]
-[""]
-["a"]
+[
