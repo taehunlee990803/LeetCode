753f3355897a668c9ea06f59773676e000cf4384
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2024-02-18 19:03:49 -0500

    Update file 2/18/2024, 7:03:49 PM

--- 231-PowerofTwo/231-PowerofTwo.py
+++ 231-PowerofTwo/231-PowerofTwo.py
@@ -2,14 +2,18 @@ class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
         
 
-        def helper(n):
+        # def helper(n):
+        #     if n == 1 or n == 2:
+        #         return True
+        #     elif n < 2:
+        #         return False
+        #     else:
+        #         return helper(n/2)
+        # return helper(n)
+        while n > 0:
             if n == 1 or n == 2:
                 return True
-            elif n < 2:
-                return False
             else:
-                return helper(n/2)
-        return helper(n)
+                n = n / 2
+        return False
 1
-16
-3
