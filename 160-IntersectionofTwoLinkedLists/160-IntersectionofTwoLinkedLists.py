47e9f8eadeecf7dcf485885e015880b7499231d5
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2024-02-18 19:52:37 -0500

    Update file 2/18/2024, 7:52:37 PM

--- 160-IntersectionofTwoLinkedLists/160-IntersectionofTwoLinkedLists.py
+++ 160-IntersectionofTwoLinkedLists/160-IntersectionofTwoLinkedLists.py
@@ -1,16 +1,29 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
 #         self.next = None
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        pA, pB = headA, headB
+        a = headA
+        b = headB
+        containerA = []
+        containerB = []
         
-            
-        while pA != pB:
-            pB = pB.next if pB else headA
-        return pA
-            pA = pA.next if pA else headB
-#         self.val = x
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
+        while a:
+            containerA.append(a)
+            a = a.next
+        while b:
+            if b in containerA:
+                return b
+            b = b.next
+        #     containerB.append(b.val)
+        #     b = b.next
+        # print(containerA, containerB)
+        # for i in range(len(containerA)):
+        #     for j in range(len(containerB)):
+        #         if containerA[i] == containerB[j]:
+        #             return containerA[i]
+        return None
 8
