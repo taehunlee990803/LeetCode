058c9f1f5ee2c7f9f27a2a168ac369156d46e783
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2024-03-10 04:06:55 -0400

    Create file

---
+++ 349-IntersectionofTwoArrays/349-IntersectionofTwoArrays.py
@@ -0,0 +1,11 @@
+
+
+        else:
+            for element in nums1:
+                if element in nums2:
+                    if element not in container:
+                        container.append(element)
+            return container
+
+        
+[
