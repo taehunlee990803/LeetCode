11e7d6a67152480399d51f518e28999a1cd5b1f1
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2024-03-11 15:19:29 -0400

    Create file

---
+++ 791-CustomSortString/791-CustomSortString.py
@@ -0,0 +1,20 @@
+    def customSortString(self, order: str, s: str) -> str:
+        junk = ""
+        temp = ""
+
+        for element in s:
+            if element not in order:
+                junk += element
+            else:
+                temp += element
+
+        s = Counter(temp)
+        for element in order:
+            if element in s:
+        ans = ""
+                ans += element * s[element]
+        ans += junk
+
+        return ans
+        # for element in 
+"
