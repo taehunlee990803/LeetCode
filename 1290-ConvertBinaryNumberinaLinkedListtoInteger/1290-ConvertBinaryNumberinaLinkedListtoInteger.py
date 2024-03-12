d871e37732888d8216b56e47f91179b935fc5785
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2024-03-12 16:56:23 -0400

    Update file 3/12/2024, 4:56:23 PM

--- 1290-ConvertBinaryNumberinaLinkedListtoInteger/1290-ConvertBinaryNumberinaLinkedListtoInteger.py
+++ 1290-ConvertBinaryNumberinaLinkedListtoInteger/1290-ConvertBinaryNumberinaLinkedListtoInteger.py
@@ -5,15 +5,14 @@
 #         self.next = next
 class Solution:
     def getDecimalValue(self, head: ListNode) -> int:
-        container = []
-        while head:
-            container.append(head.val)
-            head = head.next
-        ans = 0
-        temp = 0
-        for i in range(len(container)-1,-1,-1):
-            if container[i] == 1:
-                ans += pow(2,temp)
-            temp+=1
-        return ans 
+        ans = ''
+        if head is None:
+
+            return 
+        temp = head
+        while temp:
+            ans = ans + str(temp.val)
+            temp = temp.next
+        return int(ans,2)
+
 [
