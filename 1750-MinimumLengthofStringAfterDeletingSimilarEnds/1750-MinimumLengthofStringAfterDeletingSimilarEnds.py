b0615610af695f8b42aa32665c67fac1069f610b
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2024-03-11 21:03:47 -0400

    Update file 3/11/2024, 9:03:45 PM

--- 1750-MinimumLengthofStringAfterDeletingSimilarEnds/1750-MinimumLengthofStringAfterDeletingSimilarEnds.py
+++ 1750-MinimumLengthofStringAfterDeletingSimilarEnds/1750-MinimumLengthofStringAfterDeletingSimilarEnds.py
@@ -1,27 +1,25 @@
+        #         if l == r:
+        #             return 1
+        #         while ans[l] == temp and l < len(s)-1:
+        #             l+=1
+        #         return r-l+1
         #     else:
         #         temp = ans[l]
-                return r-l+1
-            if ans[l] != ans[r]:
-            return 1
-        while l <= r:
-        if len(s) < 2:
-        ans = s
-            else:
-                temp = ans[l]
-                while ans[l] == temp and l < len(s)-1:
+        #     if ans[l] != ans[r]:
+        # while l <= r:
+        # if len(s) < 2:
+        #     return 1
+        # l, r = 0, len(s)-1
+        # ans = s
+                r-=1
+        return r-l+1
+            while l<=r and s[l] == temp:
+                l+=1
+            while r >= l and s[r] == temp:
 
-                    l+=1
-                if l > r:
-                    return 0
-                
-                while ans[r] == temp and r>0:
-                    r -=1
-                if l > r:
-                    return 0
-        return r - l +1
-        #         while ans[l] == temp and l <= r and l<len(s)-1:
-        #             l+=1
-        #         if l >= len(s)-1:
-                if l == r:
-                    return 1
+        while l<r and s[l] == s[r]:
+            temp = s[l]
+class Solution:
+    def minimumLength(self, s: str) -> int:
+        l, r= 0,len(s)-1
 "
