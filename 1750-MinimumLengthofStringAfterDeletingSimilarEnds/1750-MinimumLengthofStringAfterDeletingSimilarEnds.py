bcac0bdfd792abd11515f440cfc2a50d339b1e9f
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2024-03-11 21:02:35 -0400

    Create file

---
+++ 1750-MinimumLengthofStringAfterDeletingSimilarEnds/1750-MinimumLengthofStringAfterDeletingSimilarEnds.py
@@ -0,0 +1,27 @@
+        #     else:
+        #         temp = ans[l]
+                return r-l+1
+            if ans[l] != ans[r]:
+            return 1
+        while l <= r:
+        if len(s) < 2:
+        ans = s
+            else:
+                temp = ans[l]
+                while ans[l] == temp and l < len(s)-1:
+
+                    l+=1
+                if l > r:
+                    return 0
+                
+                while ans[r] == temp and r>0:
+                    r -=1
+                if l > r:
+                    return 0
+        return r - l +1
+        #         while ans[l] == temp and l <= r and l<len(s)-1:
+        #             l+=1
+        #         if l >= len(s)-1:
+                if l == r:
+                    return 1
+"
