02e85fdf094a6cabc6836dd8872066cb55f8f48e
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2024-03-14 19:31:39 -0400

    Create file

---
+++ 1708-LargestSubarrayLengthK/1708-LargestSubarrayLengthK.py
@@ -0,0 +1,13 @@
+            temp = nums[i:i+k]
+            if firstValue > maxValue:
+                ans = temp
+    def largestSubarray(self, nums: List[int], k: int) -> List[int]:
+        container = []
+        for i in range(len(nums)-k+1):
+            firstValue = temp[0]
+                maxValue = firstValue
+        return ans
+class Solution:
+        maxValue = 0
+        
+[
