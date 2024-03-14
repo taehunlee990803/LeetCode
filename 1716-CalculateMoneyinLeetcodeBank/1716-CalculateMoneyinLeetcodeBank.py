9f07e0d48e6fd753dd6f47d5c4bdee2ddbe82087
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2024-03-14 13:16:03 -0400

    Create file

---
+++ 1716-CalculateMoneyinLeetcodeBank/1716-CalculateMoneyinLeetcodeBank.py
@@ -0,0 +1,22 @@
+class Solution:
+    def totalMoney(self, n: int) -> int:
+        total = 0
+        week = 0
+        for i in range(1,n+1):
+
+
+        temp = 1
+            total += (k)
+        return total
+            temp += 1
+            print(i, k)
+            k = week+temp
+        count = 0
+            if count == 7:
+                week += 1
+                temp = 1
+
+            count += 1
+                count = 0
+        
+4
