d3e2eba69561d33fdf6586b1a4dc8565c00b02dd
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2024-05-02 17:28:41 -0400

    Create file

---
+++ 2441-LargestPositiveIntegerThatExistsWithItsNegative/2441-LargestPositiveIntegerThatExistsWithItsNegative.py
@@ -0,0 +1,16 @@
+
+        nums = sorted(nums)
+        while nums[temp] > 0 or temp >= 0:
+        return -1
+        temp = len(nums)-1
+
+            minus = nums[temp]*-1
+            if minus in nums:
+                return nums[temp]
+            else:
+                temp -= 1
+        # print(nums)
+
+        if nums[0] > 0:
+            return -1
+[
