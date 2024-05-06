5df830d86f9dd0dbfc99811a9b6b52865daf90b2
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2024-05-06 17:43:33 -0400

    Create file

---
+++ 165-CompareVersionNumbers/165-CompareVersionNumbers.py
@@ -0,0 +1,17 @@
+            for i in range(len(left)):
+                if left[i] > right[i]:
+        elif len(left) < len(right):
+            else:
+                return 0
+                    return 1
+                elif left[i] < right[i]:
+                    return -1
+            if sum(right) - sum(left) > 0:
+                return -1
+            else:
+                return 0
+        else:
+            for i in range(len(left)):
+                if left[i] > right[i]:
+                    return 1
+"
