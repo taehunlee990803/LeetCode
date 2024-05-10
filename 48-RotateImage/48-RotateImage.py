01b38917e4a9580935f646f375b852bf946bece9
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2024-05-10 10:57:10 +0900

    Create file

---
+++ 48-RotateImage/48-RotateImage.py
@@ -0,0 +1,18 @@
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        temp = []
+        for i in range(n):
+            for j in range(n):
+                k.append(matrix[n-j-1][i])
+            k = []
+        n = len(matrix)
+            temp.append(k)
+        for i in range(n):
+            for j in range(n):
+                matrix[i][j] = temp[i][j]
+        
+
+        
+[
