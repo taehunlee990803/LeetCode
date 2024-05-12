a93b0497bb64b5cebeed05574aae9305f682bb59
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2024-05-13 07:50:03 +0900

    Update file 5/13/2024, 7:50:03 AM

--- 2373-LargestLocalValuesinaMatrix/2373-LargestLocalValuesinaMatrix.py
+++ 2373-LargestLocalValuesinaMatrix/2373-LargestLocalValuesinaMatrix.py
@@ -1,20 +1,17 @@
+                temp.append(tempMax)
+        
+                    for l in range(j-1, j+2):
+                        tempMax = max(tempMax, grid[k][l])
+            ans.append(temp)
+        return ans 
     def largestLocal(self, grid: List[List[int]]) -> List[List[int]]:
         n = len(grid)
         ans = []
         one = 0
-                temp.append(tempMax)
-        for i in range(n):
-            for j in range(3):
-                currentMax = 0
-                temp = []
-                for x in range(3):
         for i in range(1, n-1):
             temp = []
             for j in range(1, n-1):
                 tempMax = 0
                 for k in range(i-1, i+2):
-                    for l in range(j-1, j+2):
-                        tempMax = max(tempMax, grid[k][l])
-            ans.append(temp)
-        return ans 
+class Solution:
 [
