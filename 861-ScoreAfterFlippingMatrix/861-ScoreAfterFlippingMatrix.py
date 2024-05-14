c2bc2871d12f506a4f9a3038343922e012c7a224
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2024-05-14 20:38:19 +0900

    Update file 5/14/2024, 8:38:18 PM

--- 861-ScoreAfterFlippingMatrix/861-ScoreAfterFlippingMatrix.py
+++ 861-ScoreAfterFlippingMatrix/861-ScoreAfterFlippingMatrix.py
@@ -4,14 +4,13 @@
             for j in range(n):
                 if grid[j][i] == 0:
                     zero += 1
-            zero = 0
                 for t in range(n):
                     if grid[t][i] == 0:
                         grid[t][i] = 1
                     else:
+            one = 0
+            zero = 0
+                        grid[i][j] = 0
+        for i in range(k):
                         grid[t][i] = 0
-        ans = 0
-        print(grid)
-        for i in range(n):
-            temp = ''
 [
