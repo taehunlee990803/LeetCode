6f2c68292b4b5097fb794bb38ff2efd654bfebd0
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2024-05-15 17:59:26 +0900

    Create file

---
+++ 246-StrobogrammaticNumber/246-StrobogrammaticNumber.py
@@ -0,0 +1,20 @@
+        container = [1,6,8,9]
+        l,r = 0,len(temp)-1
+        while l <= r:
+            elif temp[l] == '1':
+                if temp[r] == '1':
+                    l += 1
+                    r -= 1
+            print(element)
+            if int(element) in op:
+                return False
+            else:
+                temp += (element)
+            if temp[l] == '0':
+                if temp[r] == '0':
+                else:
+                    l += 1
+                    r -= 1
+                else:
+                    return False
+"
