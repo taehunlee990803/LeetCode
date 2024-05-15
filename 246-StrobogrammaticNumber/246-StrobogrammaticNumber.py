52fedf39f974635c6952ec17d8dbac3e3cab1a36
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2024-05-15 18:00:36 +0900

    Update file 5/15/2024, 6:00:35 PM

--- 246-StrobogrammaticNumber/246-StrobogrammaticNumber.py
+++ 246-StrobogrammaticNumber/246-StrobogrammaticNumber.py
@@ -1,20 +1,21 @@
-        container = [1,6,8,9]
-        l,r = 0,len(temp)-1
-        while l <= r:
             elif temp[l] == '1':
-                if temp[r] == '1':
-                    l += 1
-                    r -= 1
-            print(element)
-            if int(element) in op:
-                return False
-            else:
-                temp += (element)
-            if temp[l] == '0':
-                if temp[r] == '0':
-                else:
-                    l += 1
-                    r -= 1
-                else:
+                if temp[r] != '1':
                     return False
+                    return False
+            elif temp[l] == '6':
+                if temp[r] != '9':
+                    return False
+            elif temp[l] == '8':
+                if temp[r] != '8':
+                    return False
+            elif temp[l] == '9':
+                    return False
+            l += 1
+
+        return True            
+            r -= 1
+                if temp[r] != '6':
+        while l <= r:
+            if temp[l] == '0':
+                if temp[r] != '0':
 "
