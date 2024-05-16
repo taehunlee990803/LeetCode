6de287933e9316d2f980253e63c2af2582754e00
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2024-05-16 09:25:31 +0900

    Update file 5/16/2024, 9:25:31 AM

--- 2331-EvaluateBooleanBinaryTree/2331-EvaluateBooleanBinaryTree.py
+++ 2331-EvaluateBooleanBinaryTree/2331-EvaluateBooleanBinaryTree.py
@@ -1,5 +1,3 @@
-                else:
-                    return False
             elif root.val == 2:
                 return self.helper(root.left) or self.helper(root.right)
             elif root.val == 3:
@@ -7,10 +5,16 @@
             else:
                 return False
     def evaluateTree(self, root: Optional[TreeNode]) -> bool:
+            if root.val == 0 or root.val == 1:
+                return root.val == 1
+class Solution:
+    def helper(self, root):
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 
 
         if root is None:
-            return False
-
-        return self.helper(root)
 [
