ca7d4f1f09c0c3d135c04a1a078c755e678a862c
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2024-05-16 10:03:14 +0900

    Create file

---
+++ 953-VerifyinganAlienDictionary/953-VerifyinganAlienDictionary.py
@@ -0,0 +1,26 @@
+            # firstLetter = words[i][0]
+        for i in range(n):
+
+            container[element] = i
+        # print(container)
+        n = len(words)
+        container = {}
+        for i,element in enumerate(order):
+
+
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+            return False
+                return True
+        if len(current) > len(next):
+        else:
+            return True
+            for j in range(i+1, n):
+                if words[i] != words[j]:
+                    if not self.is_valid(words[i],words[j],container):
+                        return False
+        return True
+            #     currentFirst = words[j][0]
+            #     if container[firstLetter] > container[currentFirst]:
+            #         return False
+            #     elif container[firstLetter] == container[currentFirst]:
+[
