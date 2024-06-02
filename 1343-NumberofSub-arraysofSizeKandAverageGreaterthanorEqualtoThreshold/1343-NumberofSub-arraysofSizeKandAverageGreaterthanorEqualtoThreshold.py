9cf299da198b5a9dda9ecaa8aa9a1bda704439f6
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2024-06-02 14:06:09 +0900

    Update file 6/2/2024, 2:06:09 PM

--- 1343-NumberofSub-arraysofSizeKandAverageGreaterthanorEqualtoThreshold/1343-NumberofSub-arraysofSizeKandAverageGreaterthanorEqualtoThreshold.py
+++ 1343-NumberofSub-arraysofSizeKandAverageGreaterthanorEqualtoThreshold/1343-NumberofSub-arraysofSizeKandAverageGreaterthanorEqualtoThreshold.py
@@ -1,15 +1,19 @@
         while l < len(arr)-k:
             if currentSum >= temp:
                 count += 1
-
             currentSum += arr[l+k]
             currentSum -= arr[l]
-            l += 1
         currentSum = sum(arr[l:l+k])
-            # print(currentSum)
-            print(currentSum)
+            l += 1
 
         if currentSum >= temp:
                 count += 1
         return count 
+
+        count = 0
+        l = 0
+        currentSum = sum(arr[l:l+k])
+    def numOfSubarrays(self, arr: List[int], k: int, threshold: int) -> int:
+        temp = threshold*k
+class Solution:
 [
