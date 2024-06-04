8a0dff03ab8be249f65651ae78edf3459ad5a04b
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2024-06-04 10:09:35 +0900

    Update file 6/4/2024, 10:09:34 AM

--- 409-LongestPalindrome/409-LongestPalindrome.py
+++ 409-LongestPalindrome/409-LongestPalindrome.py
@@ -1,27 +1,15 @@
-    def longestPalindrome(self, s: str) -> int:
-        if len(s) == 1:
-            return 1
-        dic = {}
-        for alpha in s:
-            if alpha in dic:
-                dic[alpha] += 1
-            else:
-                dic[alpha] = 1
-        count = 0
-
-        for element in dic:
-            if dic[element] %2 == 0:
-                count += dic[element]
-            else:
+class Solution:
+    def appendCharacters(self, s: str, t: str) -> int:
+        ans = len(t)
+        r = 0
+        for element in s:
+            if element == t[r]:
+                ans -= 1
+            
+        # print(ans, maxNum)
+                if ans == 0:
+                    return 0
         temp = 0
-                temp = 1
-        if temp == 1:
-            return count + 1
-        else:
-        if len(dic) == 1:
-            return len(s)
-                current = dic[element]
-                while current > 2:
-                    count += 2
-                    current -= 2
+                r += 1
+        return ans
 "
