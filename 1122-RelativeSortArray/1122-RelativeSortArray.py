e6ff71b6b0852db9140e2e82ed8df65ff4de3120
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2024-06-11 18:39:10 +0900

    Create file

---
+++ 1122-RelativeSortArray/1122-RelativeSortArray.py
@@ -0,0 +1,21 @@
+        for element in arr1:
+            if element in dic:
+                dic[element] += 1
+            else:
+                dic[element] = 1
+        for element in arr2:
+        container = []
+                container.append(element)
+            if element in dic:
+                k = dic[element]
+                for i in range(k):
+                    ans.append(element)
+        for element in container:
+        container = sorted(container)
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        ans = []
+        dic = {}
+class Solution:
+        print(container)
+                container.remove(element)
+[
