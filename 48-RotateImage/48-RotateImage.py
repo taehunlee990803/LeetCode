c9d9d8328ccf36a48b56423e3c7dacb624bd47bf
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2024-12-15 17:10:49 -0500

    Update file 12/15/2024, 5:10:49 PM

--- 48-RotateImage/48-RotateImage.py
+++ 48-RotateImage/48-RotateImage.py
@@ -1,18 +1,23 @@
+
+
+            temp.append(k)
+        
+                k.append(matrix[n-j-1][i])
+        for i in range(n): # 0,1,2
+            k = []
+            for j in range(n): # 0,1,2
+        n = len(matrix)
+
+        """
+
+        temp = []
+class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
-        """
-        temp = []
-        for i in range(n):
-            for j in range(n):
-                k.append(matrix[n-j-1][i])
-            k = []
-        n = len(matrix)
-            temp.append(k)
-        for i in range(n):
+
+
+        for i in range(n): 
             for j in range(n):
                 matrix[i][j] = temp[i][j]
-        
 
-        
-[
