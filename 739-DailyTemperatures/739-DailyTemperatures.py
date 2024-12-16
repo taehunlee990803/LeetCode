640588c48b77b7b7ec7c06ca93883257698ab4d0
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2024-12-15 22:30:16 -0500

    Update file 12/15/2024, 10:30:16 PM

--- 739-DailyTemperatures/739-DailyTemperatures.py
+++ 739-DailyTemperatures/739-DailyTemperatures.py
@@ -2,11 +2,16 @@ class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List
 [int]:
         n = len(temperatures)
-        result = [0]*n
-        stack = []
-        for index, temp in enumerate(temperatures):
-            while stack and temperatures[stack[-1]] < temp:
-                prev_temp = stack.pop()
-                result[prev_temp] = index - prev_temp
-            stack.append(index)
+        result = [0] * n  # Initialize the result array with all 
+
+        # Iterate through each day
+        for i in range(n):
+            # Check all future days
+            for j in range(i + 1, n):
+                if temperatures[j] > temperatures[i]:
+zeros
+                    result[i] = j - i  # Store the difference in 
+days
+                    break  # Exit the loop once we find a warmer day
+
         return result
