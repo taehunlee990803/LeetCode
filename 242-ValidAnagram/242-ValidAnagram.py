60b42ceb9d7191861abac3578cf22a0c10b5af45
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2024-12-22 18:07:00 -0500

    Update file 12/23/2024, 8:07:00 AM

--- 242-ValidAnagram/242-ValidAnagram.py
+++ 242-ValidAnagram/242-ValidAnagram.py
@@ -2,7 +2,7 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-        for idex in set(s):
+        for idx in set(s):
             if s.count(idx) != t.count(idx):
                 return False
         return True
