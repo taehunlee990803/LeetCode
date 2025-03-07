a88bf79b691b5a2963879d0493cfc5aaa6d79f35
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2025-03-07 00:49:06 -0500

    Update file 3/7/2025, 2:49:06 PM

--- 448-FindAllNumbersDisappearedinanArray/448-FindAllNumbersDisappearedinanArray.py
+++ 448-FindAllNumbersDisappearedinanArray/448-FindAllNumbersDisappearedinanArray.py
@@ -1,26 +1,11 @@
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-        hash_table = {}
-        # for element in nums_set:
-        # nums_set = set(nums.sort())
-        #     if element != 
-        # temp =
-        
-        # Add each of the numbers to the hash table
-        for num in nums:
-            hash_table[num] = 1
-        
-        # Response array that would contain the missing numbers
-        result = []    
-        
-        # Iterate over the numbers from 1 to N and add all those
-        # that don't appear in the hash table. 
-        for num in range(1, len(nums) + 1):
-            if num not in hash_table:
-                result.append(num)
-                
-        return result        
-        # return ans 
-        # max
-        # ans = []
-[
+        n = len(nums)
+        set_nums = set(nums)
+        ans = []
+        temp = [i for i in range(1, n+1)]
+        for element in temp:
+            if element not in set_nums:
+                ans.append(element)
+        return ans 
+        # print(temp, set_nums)
