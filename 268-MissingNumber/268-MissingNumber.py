7749d9881f61974335581a852d0ea0654fa64963
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2025-03-07 00:32:54 -0500

    Update file 3/7/2025, 2:32:54 PM

--- 268-MissingNumber/268-MissingNumber.py
+++ 268-MissingNumber/268-MissingNumber.py
@@ -1,9 +1,10 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        temp = [i for i in range(0, n+1)]
+
+        for element in nums:
+            temp.remove(element)
+        # return temp - nums
+        return temp[0]
         
-        for i in range(len(nums)):
-        nums.sort()
-            if nums[i] != i:
-                return i
-        return len(nums)
-[
