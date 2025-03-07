8870eccd25969f37902f87aa3e2d2858e4082dba
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2025-03-07 00:56:53 -0500

    Update file 3/7/2025, 2:56:53 PM

--- 448-FindAllNumbersDisappearedinanArray/448-FindAllNumbersDisappearedinanArray.py
+++ 448-FindAllNumbersDisappearedinanArray/448-FindAllNumbersDisappearedinanArray.py
@@ -3,9 +3,9 @@ class Solution:
         n = len(nums)
         set_nums = set(nums)
         ans = []
-        temp = [i for i in range(1, n+1)]
-        for element in temp:
-            if element not in set_nums:
-                ans.append(element)
+        # temp = [i for i in range(1, n+1)]
+        for i in range(1, n+1):
+            if i not in set_nums:
+                ans.append(i)
         return ans 
         # print(temp, set_nums)
