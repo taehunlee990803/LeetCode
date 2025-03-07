86ddda056ebfe527a2b277a58fca0f08e42b91b6
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2025-03-06 23:45:43 -0500

    Update file 3/7/2025, 1:45:40 PM

--- 605-CanPlaceFlowers/605-CanPlaceFlowers.py
+++ 605-CanPlaceFlowers/605-CanPlaceFlowers.py
@@ -1,21 +1,33 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        
-
-        total_length = len(flowerbed)
-
-
-        for i in range(total_length):
-            if flowerbed[i] == 0:
-                left_side = (i == 0) or (flowerbed[i-1] == 0)
-                right_side = (i == total_length-1) or (flowerbed[i+1] == 0)
-
-                if left_side and right_side:
-                    n -= 1 
+        possible = 0
+            if i == 0:
+            elif i == len(flowerbed)-1:
+                    possible += 1
                     flowerbed[i] = 1
+                    possible += 1
+                    flowerbed[i] = 1
+            else:
+                if flowerbed[i-1] == 0 and flowerbed[i+1] == 0 and flowerbed[i] == 0:
+                    possible += 1
+                    flowerbed[i] = 1
+        return possible >= n
+                if flowerbed[i] == 0 and flowerbed[i-1] == 0:
+                if flowerbed[i] == 0 and flowerbed[i+1] == 0:
+        
+            print(i, possible)
+        if len(flowerbed) == 1:
+            if flowerbed[0] == 1:
+                if n == 0:
+            else:
+        for i in range(len(flowerbed)):
+                if n <= 1:
+                    return True
+                else:
+                    return False
+                    return True
+                else:
+                    return False
 
-                    if n == 0:
-                        return True
-        return n <= 0
 
         
