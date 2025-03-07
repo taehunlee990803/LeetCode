6c628e972c5a778992e158ad48787daa895d2cb2
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2025-03-07 00:35:23 -0500

    Update file 3/7/2025, 2:35:23 PM

--- 268-MissingNumber/268-MissingNumber.py
+++ 268-MissingNumber/268-MissingNumber.py
@@ -3,8 +3,12 @@ class Solution:
         n = len(nums)
         temp = [i for i in range(0, n+1)]
 
-        for element in nums:
-            temp.remove(element)
+        # for element in nums:
+        #     temp.remove(element)
         # return temp - nums
-        return temp[0]
+        # return temp[0]
+
+        s1 = sum(nums)
+        s2 = sum(range(n+1))
+        return s2-s1
         
