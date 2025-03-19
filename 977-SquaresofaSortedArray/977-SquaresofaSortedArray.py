904b4a499066c1341450982934d977cdb7f3b461
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2025-03-19 04:02:10 -0400

    Create file

---
+++ 977-SquaresofaSortedArray/977-SquaresofaSortedArray.py
@@ -0,0 +1,27 @@
+        def merge(A, B):
+            a = b = 0
+            ans = []
+
+            while a < len(A) and b < len(B):
+                if A[a] < B[b]:
+                    ans.append(A[a])
+                    a+=1
+
+        print(neg, pos)
+
+        pos = [item for item in pos]
+        neg = [item*-1 for item in reversed(neg)]
+
+            pos = nums[idx:]
+                break
+            neg = nums[:idx]
+                idx = i
+            if nums[i] >= 0:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        idx = -1
+        for i in range(len(nums)):
+class Solution:
+        if idx == -1: # every element are negative
+            return [n**2 for n in reversed(nums)]
+        else:
+                else:
