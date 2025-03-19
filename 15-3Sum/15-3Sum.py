1eb702a37623ec8ef499be94e81695d24a52eac9
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2025-03-19 09:40:37 -0400

    Update file 3/19/2025, 10:40:36 PM

--- 15-3Sum/15-3Sum.py
+++ 15-3Sum/15-3Sum.py
@@ -1,30 +1,28 @@
-class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        dic = Counter(nums)
-        ans = set()
-        nums.sort()
-        for i in range(0,len(nums)):
+            r = len(nums)-1
+        
+            while l < r:
+                tempSum = val + nums[l] + nums[r]
+                # print(i, l, r)
+                # print( current, nums[l], nums[j])
+                # print("sum:", tempSum)
+                if tempSum == 0:
+                    temp = [val, nums[l], nums[r]]
+                    # if sorted(temp) not in ans:
+                    #     ans.append(sorted(temp))
+                    l += 1
+                    # ans.append([current, nums[l], nums[j]])
+                    # break
+            l = i+1
+                continue
+            if (i > 0) and val == nums[i-1]:
             # for j in range(i+1, len(nums)):
-            #     temp = nums[i] + nums[j]
-              
-            #     temp_minus = temp*-1
-
-            #     if nums[j+1] > temp_minus:
-            #         break
-            second = i + 1
-            while second < last:
-                temp = nums[first] + nums[second] + nums[last]
-            last = len(nums) -1
-                if temp == 0:
-
-                    ans.add((nums[first], nums[second], nums[last]))
-                    second += 1
-                    last -= 1
-                elif temp > 0:
-                    last -= 1
-                else:
-                    second += 1
-        return ans
-
-            first = i
-[
+            
+        # for i in range(len(nums)):
+         
+        # print(nums)
+        nums = sorted(nums)
+        ans = []
+        for i, val in enumerate(nums):
+                    while (l < r) and nums[l] == nums[l-1]:
+                    # temp.append()
+                    ans.append(temp)
