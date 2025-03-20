aef3117c62ea340abbcb2d8d2ac388071138ece2
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2025-03-19 22:14:25 -0400

    Create file

---
+++ 136-SingleNumber/136-SingleNumber.py
@@ -0,0 +1,11 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        dic = dict()
+        for element in nums:
+            if element not in dic:
+                dic[element] = 1
+            else:
+                dic[element] += 1
+        for element in dic:
+            if dic[element] == 1:
+                return element
