fa9f4ca6df662f16e3b622e44b7322d889fe8e33
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2025-03-19 22:53:53 -0400

    Update file 3/20/2025, 11:53:53 AM

--- 70-ClimbingStairs/70-ClimbingStairs.py
+++ 70-ClimbingStairs/70-ClimbingStairs.py
@@ -1,12 +1,19 @@
-class Solution:
-    def climbStairs(self, n: int) -> int:
-        dp = [0] * (n + 1)
-        dp[0] = 1
-        dp[1] = 1
+        # dp = [0] * (n + 1)
+        # dp[0] = 1
+        # dp[1] = 1
 
-        for i in range(2, n + 1):
+        # for i in range(2, n + 1):
+        for i in range(2, n+1):
             dp[i] = dp[i-1] + dp[i-2]
-        
+        # 3
+        # 1 + 1 + 1 = 2 + 1 = 1 + 2
         return dp[-1]
+        # dp[3] = dp[1] + dp[2] = 2 + 1 = 3
+        # dp[2] = dp[0] + dp[1] = 1 + 1 = 2
 
+        dp[1] = 1
+        dp[0] = 1
+        dp = [0] * (n+1)
 
+    def climbStairs(self, n: int) -> int:
+class Solution:
