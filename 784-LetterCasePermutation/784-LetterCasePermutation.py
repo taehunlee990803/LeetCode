e14aab8dd4e135a82d6a21353f8af1c2a0124256
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2025-03-27 14:18:31 -0400

    Create file

---
+++ 784-LetterCasePermutation/784-LetterCasePermutation.py
@@ -0,0 +1,20 @@
+                    tmp.append(a+c.lower())
+                    tmp.append(a+c.upper())
+                    # print("tmp: ", tmp)
+                    # print("------")
+            else:
+                for a in ans:
+                    tmp.append(a+c)
+            ans = tmp
+            if c.isalpha():
+                for a in ans:
+                    # print("------")
+                    # print("a in ans", a, ans)
+            tmp = []
+            # print("C: ", c)
+        for c in s:
+            print("Current Output: ", ans)
+            
+        return ans 
+
+                  
