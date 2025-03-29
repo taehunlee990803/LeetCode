34b64f563ba90035eac9e845d0fc2a4c26aebd9f
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2025-03-29 15:24:37 -0400

    Update file 3/29/2025, 3:24:36 PM

--- 21-MergeTwoSortedLists/21-MergeTwoSortedLists.py
+++ 21-MergeTwoSortedLists/21-MergeTwoSortedLists.py
@@ -4,14 +4,15 @@
             if list1.val < list2.val:
             else:
                 current.next = list1
-                list1, current = list1.next, list1
+                list1 = list1.next
                 current.next = list2
-                list2, current = list2.next, list2
+                list2 = list2.next
         if list1:
             current.next = list1
         else:
             current.next = list2
         return dummy.next
-
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-class Solution:
+                current = current.next
+                current = current.next
+
