fe066ca6f685b8639a3d80945b059c2d2a0d49ab
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2025-03-29 10:59:07 -0400

    Create file

---
+++ 46-Permutations/46-Permutations.py
@@ -0,0 +1,17 @@
+        if len(nums) == 1:
+            return [nums.copy()]
+
+        for i in range(len(nums)):
+            n = nums.pop(0)
+
+            perms = self.permute(nums) # perms = [[2,3], [3,2]]
+
+            nums.append(n)
+            for perm in perms:
+                perm.append(n)
+            res.extend(perms)
+        return res
+
+        res = []
+    def permute(self, nums: List[int]) -> List[List[int]]:
+class Solution:
