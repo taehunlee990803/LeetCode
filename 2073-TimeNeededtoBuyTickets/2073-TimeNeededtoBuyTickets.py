585dfc13d7b5abf8f587b90363017766e6f7d146
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2025-03-30 13:16:48 -0400

    Update file 3/30/2025, 1:16:48 PM

--- 2073-TimeNeededtoBuyTickets/2073-TimeNeededtoBuyTickets.py
+++ 2073-TimeNeededtoBuyTickets/2073-TimeNeededtoBuyTickets.py
@@ -1,10 +1,16 @@
-class Solution:
+        #     if i <= k:
+        #         count += min(tickets[k], tickets[i])
+        #     else:
+        #         count += min(tickets[k]-1, tickets[i])
+        # return count 
+        time = 0
+        while True:
+            for i in range(len(tickets)):
+                if tickets[k] == 0:
+                    return time
+                if tickets[i] >= 1:
+                    tickets[i] -= 1
+        # for i in range(len(tickets)):
+        # count = 0
     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
-        count = 0
-        for i in range(len(tickets)):
-            if i <= k:
-                count += min(tickets[k], tickets[i])
-            else:
-                count += min(tickets[k]-1, tickets[i])
-        return count 
-
+class Solution:
