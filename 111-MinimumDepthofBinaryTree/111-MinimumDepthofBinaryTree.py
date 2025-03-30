ef2adb77c4aa0b7ff59bc30fef634e6a3cda81f4
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2025-03-30 14:06:15 -0400

    Update file 3/30/2025, 2:06:15 PM

--- 111-MinimumDepthofBinaryTree/111-MinimumDepthofBinaryTree.py
+++ 111-MinimumDepthofBinaryTree/111-MinimumDepthofBinaryTree.py
@@ -1,14 +1,16 @@
-            for i in range(len(queue)):
-                node = queue.popleft()
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
-                if not node.left and not node.right:
-                    # min_ans = min(min_ans, current)
-            level += 1
-                    return level
-            # current += 1
-        # return min_ans
-
-        
+            #     if node.right:
+            #         queue.append(node.right)
+            #     if not node.left and not node.right:
+            #         # min_ans = min(min_ans, current)
+            #         return level
+            node, level = queue.popleft()
+            if node.left:
+                queue.append((node.left, level+1))
+            #         queue.append(node.left)
+            #     if node.left:
+            #     node = queue.popleft()
+            # for i in range(len(queue)):
+            # level += 1
+        while queue:
+        # min_ans = current
+        queue = deque([(root, 1)])
