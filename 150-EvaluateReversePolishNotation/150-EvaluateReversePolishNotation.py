94b9347508541883ec68d5c2a09bbcbd1a363461
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2025-03-30 06:17:41 -0400

    Update file 3/30/2025, 6:17:40 AM

--- 150-EvaluateReversePolishNotation/150-EvaluateReversePolishNotation.py
+++ 150-EvaluateReversePolishNotation/150-EvaluateReversePolishNotation.py
@@ -1,30 +1,23 @@
-        return int(num_stack[0])
-
-        # if len(num_stack) == 1:
-                num_stack.append(element)
-            
-            else:
-                num_stack.append(ans)
-        
-                    ans = temp1 * temp2
-                else:
-                    ans = int(temp1/temp2)
-                elif element == '-':
-                    ans = temp1 - temp2
-                elif element == '*':
-        operator_stack = []
-        operator = ['+', '-','*','/']
-        for element in tokens:
-            if element in operator:
-                temp2 = int(num_stack.pop())
-                temp1 = int(num_stack.pop())
-                if element == '+':
-                    ans = temp1 + temp2
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        num_stack = []
-               ·····#·if·ans·<·0:
-····················#·····ans·=·0
+        # exp = []
+        # num = []
+        # ans = 1
+        # valid_op = ['+', '-', '/', '*']
 
-        # while operator_stack:
-[
+        stack = []
+        for t in tokens:
+            if t not in valid_op:
+        valid_op = ['+', '-', '/', '*']
+                stack.append(int(t))
+            else:
+                r,l = stack.pop(), stack.pop()
+                if t == '+':
+                elif t == '-':
+                elif t == '*':
+                elif t == '/':
+                    stack.append(l+r)
+                    stack.append(l-r)
+                    stack.append(l*r)
+                    stack.append(int(float(l)/r))
+        return stack.pop()
