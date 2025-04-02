e27325947a85b6421b19df5a87de9f4466c4cefa
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2025-04-02 16:41:25 -0400

    Update file 4/2/2025, 4:41:25 PM

--- 108-ConvertSortedArraytoBinarySearchTree/108-ConvertSortedArraytoBinarySearchTree.py
+++ 108-ConvertSortedArraytoBinarySearchTree/108-ConvertSortedArraytoBinarySearchTree.py
@@ -1,33 +1,33 @@
-        # if len(nums) % 2 == 0:
-        # else:
-        #     mid = (len(nums) + 1)// 2
-        #     mid = len(nums) // 2
-
-        # root = TreeNode(nums[mid])
-        # left = nums[:mid]
-        # right = nums[mid:]
-
-        # for i in range(len(left)-1,-1,-1):
-        # first = True
-
-        #     if first:
-        #     temp_node = TreeNode(left[i])
-        root = TreeNode(nums[mid])
-
-
-        root.left = self.sortedArrayToBST(nums[:mid])
-        root.right = self.sortedArrayToBST(nums[mid+1:])
-
-        return root
-        mid = len(nums) // 2
-
-            return None
 #         self.right = right
 class Solution:
     def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
-        if not nums:
 #         self.left = left
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
+        if not nums:
+            return None
+        n = len(nums)
+        mid = n // 2
+        root = TreeNode(nums[mid])
+
+
+
+
+        q = deque()
+        q.append((root, 0, mid-1))
+        q.append((root, mid+1, n-1))
+
+        while q:
+            parent, left, right = q.popleft()
+            if left <= right:
+                mid= (left + right) // 2
+                child = TreeNode(nums[mid])
+                if nums[mid] < parent.val:
+                    parent.left = child
+                else:
+                    parent.right = child 
+                q.append((child, left, mid-1))
+                q.append((child, mid+1, right))
+        return root 
