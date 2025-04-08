1fc73c924d69cb2b15e8b5463c900c1db9d22660
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2025-04-07 21:55:56 -0400

    Update file 4/7/2025, 9:55:56 PM

--- 605-CanPlaceFlowers/605-CanPlaceFlowers.py
+++ 605-CanPlaceFlowers/605-CanPlaceFlowers.py
@@ -1,34 +1,22 @@
-class Solution:
-    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        # possible = 0
-        #     if i == 0:
-        #     elif i == len(flowerbed)-1:
-        #             possible += 1
-        #             flowerbed[i] = 1
-        #             possible += 1
-        #             flowerbed[i] = 1
-        #     else:
-        #         if flowerbed[i-1] == 0 and flowerbed[i+1] == 0 and flowerbed[i] == 0:
-        #             possible += 1
-        #             flowerbed[i] = 1
-        #         if flowerbed[i] == 0 and flowerbed[i-1] == 0:
-        #         if flowerbed[i] == 0 and flowerbed[i+1] == 0:
-        # if len(flowerbed) == 1:
-        #     if flowerbed[0] == 1:
-        #         if n == 0:
-        #     else:
-        # for i in range(len(flowerbed)):
-        #         if n <= 1:
-        #             return True
-        #         else:
-        #             return False
-        #             return True
-        #         else:
-        #             return False
+
+
+
+
+
+
+
+
+
+
+        return False 
+
+
+                n -= 1
+        if n <= 0:
+            return True
         f = [0] + flowerbed + [0]
         for i in range(1, len(f)-1):
             if f[i-1] == 0 and f[i] == 0 and f[i+1] == 0:
-                n -=1 
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
                 f[i] = 1
-
-        return n <= 0
