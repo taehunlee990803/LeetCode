8a0cd5a843428caf5cdb4bbda27794ba0a6b46e8
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2025-04-08 16:08:04 -0400

    Update file 4/8/2025, 4:08:03 PM

--- 700-SearchinaBinarySearchTree/700-SearchinaBinarySearchTree.py
+++ 700-SearchinaBinarySearchTree/700-SearchinaBinarySearchTree.py
@@ -1,19 +1,22 @@
-# Definition for a binary tree node.
-# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
 class Solution:
-    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
-        while root:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional
+[TreeNode]:
+        # if not root:
+        #     return []
+        # while root:
+        #     if root.val > val:
         if not root:
             return []
-            if root.val > val:
+        while root:
+                
+            elif root.val > val:
                 root = root.left
-            elif root.val < val:
-                root = root.right
-            else:
+            if root.val == val:
                 return root
-            
-
+            else:
+                root = root.right 
+        return None
