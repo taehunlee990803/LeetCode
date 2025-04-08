ae616a14813583b6893186906a60c66bc362f586
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2025-04-08 10:47:21 -0400

    Update file 4/8/2025, 10:47:21 AM

--- 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.py
+++ 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.py
@@ -1,24 +1,24 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        # ans = []*len(nums)
-        
 
-        left = [1] * len(nums)
-        right = [1] * len(nums)
-        for i in range(len(nums)):
 
-        prev = 1
 
-            left[i] = prev
-            prev *= nums[i]
-        
-        return ans 
-        
-        for i in range(len(nums)):
-            right[i] = prev
-            prev *= nums[len(nums)-1-i]
-        prev = 1
-        ans = [1] * len(nums)
 
-        for i in range(len(nums)):
-            ans[i] = (left[i] * right[len(nums)-1-i])
+
+
+
+
+
+
+
+
+            res = [1] * len(nums)
+            prefix = 1
+            for i in range(len(nums)):
+                res[i] = prefix
+                prefix *= nums[i]
+            postfix = 1
+            for i in range(len(nums)-1,-1,-1):
+                res[i] *= postfix
+                postfix *= nums[i]
+            return res 
