47878c0321de1a7ed3913594e101490ff342acf3
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2025-04-07 22:02:48 -0400

    Update file 4/7/2025, 10:02:48 PM

--- 345-ReverseVowelsofaString/345-ReverseVowelsofaString.py
+++ 345-ReverseVowelsofaString/345-ReverseVowelsofaString.py
@@ -1,18 +1,22 @@
-class Solution:
     def reverseVowels(self, s: str) -> str:
         vowels = ['a', 'e','i','o','u']
+
+
+
+
+class Solution:
+        for element in s :
+            if element in vowels or element.lower() in vowels:
         temp = []
-        for element in s:
-            if element.lower() in vowels:
                 temp.append(element)
         for i in range(len(s)):
-            if s[i].lower() in vowels:
-                ans += temp[-1-n]
-        n = 0
-                n += 1
-        return ans
-        print(temp)
+            if s[i] in vowels or  s[i].lower() in vowels:
+                # s[i] = temp[k]
+        k = len(temp)-1
+                k-=1
+        return ans 
         ans = ""
             else:
                 ans += s[i]
-
+                ans += temp[k]
+        print(temp)
