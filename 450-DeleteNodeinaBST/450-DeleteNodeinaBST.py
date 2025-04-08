bb0f78e9bba7949ec782627291c0a37f6e762f2b
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2025-04-08 16:31:46 -0400

    Update file 4/8/2025, 4:31:46 PM

--- 450-DeleteNodeinaBST/450-DeleteNodeinaBST.py
+++ 450-DeleteNodeinaBST/450-DeleteNodeinaBST.py
@@ -1,24 +1,22 @@
-                child = current.right
+            if successor_parent.left == successor:
+                successor_parent.left = successor.right
+            else:
+                successor_parent.right = successor.right 
+            
+        else: # current.left or current.right:
+            if current.left:
+                child = current.left
+            else:
+                child = current.right 
             if not parent:
                 return child
             if parent.left == current:
                 parent.left = child
             else:
-                parent.right = child
+                parent.right = child 
+        return root  
 
-        else: # Node has left and right children
-            # Need to find the smallest node in the right subtree
-            successor_parent = current
-            successor = current.right
-            while successor.left:
-                successor_parent = successor
-                successor = successor.left
-            current.val = successor.val
 
-            if successor_parent.left == successor:
-                successor_parent.left = successor.right
 
-            else:
-                successor_parent.right = successor.right
-        return root 
+
 
