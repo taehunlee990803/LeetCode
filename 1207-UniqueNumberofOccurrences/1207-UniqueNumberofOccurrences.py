ee76940e3452fc149c679a6b051046bdb7a12c2d
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2025-04-08 12:07:43 -0400

    Update file 4/8/2025, 12:07:43 PM

--- 1207-UniqueNumberofOccurrences/1207-UniqueNumberofOccurrences.py
+++ 1207-UniqueNumberofOccurrences/1207-UniqueNumberofOccurrences.py
@@ -1,18 +1,24 @@
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
-        dic = {}
-        for element in arr:
-            if element not in dic:
-                dic[element] = 1
-            else:
-                dic[element] += 1
-        for element in dic:
-        container = []
-    
-            count = dic[element]
-            if count not in container:
-                container.append(count)
+        num = []
+
+
+        count = Counter(arr)
+        for element in count:
+            if count[element] not in num:
+                num.append(count[element])
             else:
                 return False
         return True 
-
