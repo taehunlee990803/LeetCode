6ad473905ad0c3ceeda21dd873cc8e192205e8c5
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2025-04-07 20:43:58 -0400

    Update file 4/7/2025, 8:43:58 PM

--- 283-MoveZeroes/283-MoveZeroes.py
+++ 283-MoveZeroes/283-MoveZeroes.py
@@ -1,20 +1,25 @@
-                            temp = nums[i]
-                            nums[i] = nums[j]
-                            nums[j] = temp
-                            break
 
-        for num in nums:
-            if num == 0:
-                check = False
-        if check:
-            return
-        else:
-            for i in range(0, len(nums)):
-                if nums[i] == 0:
-                    for j in range(i+1, len(nums)):
+
+
+
+
+
+
+
+
+
+
+
+                        # nums[i] = nums[j]
+                        # nums[j] = temp
+                        # print(nums)
+                            break
+                            nums[j] = temp
+                            nums[i] = nums[j]
                         if nums[j] != 0:
-        check = True
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-    def moveZeroes(self, nums: List[int]) -> None:
+                    for j in range(i+1, len(nums)):
+                    temp = nums[i]
+                if nums[i] == 0: 
+                # print(nums)
+            for i in range(len(nums)):
+        else:
