b4652164abb11eef131babb556f7883d5179989b
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2025-04-08 12:17:06 -0400

    Update file 4/8/2025, 12:17:05 PM

--- 1657-DetermineifTwoStringsAreClose/1657-DetermineifTwoStringsAreClose.py
+++ 1657-DetermineifTwoStringsAreClose/1657-DetermineifTwoStringsAreClose.py
@@ -1,16 +1,24 @@
-class Solution:
-    def calculateTime(self, keyboard: str, word: str) -> int:
-        dic = {}
+        c = list(a-b)
+        if len(c) != 0:
+            return False
+        a_count = Counter(word1)
+        b_count = Counter(word2)
+        a = []
+        b = []
 
-        for i in range(26):
-            dic[keyboard[i]] = i
-        
-        # time = dic[word[0]]
-        ans = dic[word[0]]
-        # for i in range()
-        for i in range(len(word)-1):
-            first = dic[word[i]]
-            second = dic[word[i+1]]
-            ans += abs(first - second)
-        return ans 
-"
+        for element in a_count:
+            # if a_count[element] not in a:
+            a.append(a_count[element])
+        for element in b_count:
+            # if b_count[element] not in b:
+            b.append(b_count[element])
+
+        a.sort()
+        b.sort()
+        b = set(word2)
+        a = set(word1)
+            return False
+        if len(word1) != len(word2):
+    def closeStrings(self, word1: str, word2: str) -> bool:
+class Solution:
+            # else:
