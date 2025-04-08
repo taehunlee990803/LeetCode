5df34c40278d38e631e6518ce60bf70b4f7597f2
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2025-04-07 22:23:42 -0400

    Update file 4/7/2025, 10:23:41 PM

--- 151-ReverseWordsinaString/151-ReverseWordsinaString.py
+++ 151-ReverseWordsinaString/151-ReverseWordsinaString.py
@@ -1,28 +1,23 @@
-class Solution:
-    def reverseWords(self, s: str) -> str:
+                    temp += s[i]
+                    i += 1
+                container.append(temp)
+            else:
+                i += 1
+        print(container)
         ans = ""
-        length = len(s)
-        for i in range(length-1,-1,-1):
-        temp = ""
-            if s[i] != " ":
-                ans += temp[::-1]
-
-
-              
-        
- 
-
-        check = False
-                check = True
-                if check:
-                    ans += " "
-                    check = False
-        return ans 
-
-        if temp != "":
+        for i in range(len(container)-1,-1,-1):
+            if i != 0:
+                ans += container[i]
+                ans += " "
+            else:
+                    # print(i, container,temp)
+                while i < len(s) and s[i] != " ":
                 temp = ""
-                # temp.append(s[i])
-                temp += s[i]
-            ans += temp[::-1]
-            elif temp != "":
+            if s[i] != " ":
+        # for i in range(len(s)):
+        while i < len(s):
+        i = 0
+                ans += container[i]
+        return ans 
+    
             
