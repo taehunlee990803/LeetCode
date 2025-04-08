5bbe10498c5fc8fff97bc0631510c39bb0442740
Brian Taehun Lee <76606438+taehunlee990803@users.noreply.github.com>
2025-04-08 17:27:27 -0400

    Create file

---
+++ 2095-DeletetheMiddleNodeofaLinkedList/2095-DeletetheMiddleNodeofaLinkedList.py
@@ -0,0 +1,22 @@
+            fast = fast.next.next
+        while fast and fast.next:
+            # prev = slow
+            slow = slow.next
+        prev = ListNode(0)
+
+        curr = head
+        
+        fast = head
+[ListNode]:
+        if not head or not head.next:
+            return None
+#         self.next = next
+class Solution:
+    def deleteMiddle(self, head: Optional[ListNode]) -> Optional
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+        prev.next = head
+        
+
+        slow = prev
